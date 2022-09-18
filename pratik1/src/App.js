c98ab2eb4cf00d5dab2adc3ee74420ccf1159bab
mina.yenilmez <mina.yenilmez@dgpaysit.com>
2022-09-18 18:40:21 +0300

    pratik1

---
+++ pratik1/src/App.js
@@ -0,0 +1,19 @@
+import Footer from "./templateSample/Footer";
+import Header from "./templateSample/Header";
+import Navbar from "./templateSample/Navbar";
+import PageContent from "./templateSample/PageContent";
+
+
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Header/>
+      <PageContent/>      
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ pratik1/src/templateSample/PageContent.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import About from './About'
+import Contact from './Contact'
+import ImageLocation from './ImageLocation'
+import ProjectSection from './ProjectSection'
+
+function PageContent() {
+  return (
+    <>
+    <ProjectSection/>
+    <About/>
+    <Contact/>
+    <ImageLocation/>
+    </>
+  )
+}
+
+export default PageContent
